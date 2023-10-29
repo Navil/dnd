3dd763c767d5abf164c38b274b1dcca813ce86e3
Thomas Anderl <thomas.anderl@aon.at>
2023-10-29 15:00:10 +0100

    Add image upload

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,7 +2,7 @@ import 'package:dnd/services/auth_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart' hide Provider;
 
-final authStateProvider = StreamProvider(
+final authUserProvider = StreamProvider(
     (ref) => Supabase.instance.client.auth.onAuthStateChange.map((event) {
           return event.session?.user;
         }));
