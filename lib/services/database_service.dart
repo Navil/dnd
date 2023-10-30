2a403352d368a7157d9d8f3ea5c25363686700fa
Thomas Anderl <thomas.anderl@aon.at>
2023-10-30 11:04:02 +0100

    WIP

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,9 +1,11 @@
+import 'package:dnd/model/group.dart';
 import 'package:dnd/model/player.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class DatabaseService {
   final String uid;
   final playerDatabase = Supabase.instance.client.from("players");
+  final groupDatabase = Supabase.instance.client.from("groups");
 
   DatabaseService(this.uid);
 
@@ -18,15 +20,31 @@ class DatabaseService {
 
   Future<Player?> loadPlayer(String uid) async {
     try {
-      final response = await playerDatabase
-        .select()
-        .eq('id', uid)
-        .single();
+      final response = await playerDatabase.select().eq('id', uid).single();
 
-    return Player.fromJson(response);
+      return Player.fromJson(response);
     } catch (err) {
       print(err);
     }
     return null;
+  }
+
+  Future<Group?> loadGroup(int id) async {
+    try {
+      final response = await groupDatabase.select().eq('id', id).single();
+
+      return Group.fromJson(response);
+    } catch (err) {
+      print(err);
+    }
+    return null;
+  }
+
+  Future<void> saveGroup(Group group) async {
+    try {
+      await groupDatabase.upsert(group.toJson());
+    } catch (err) {
+      print(err);
+    }
   }
 }
