9e359f0ab6308140a9789666e4ab86b6042a51a0
Thomas Anderl <thomas.anderl@aon.at>
2023-11-10 09:09:07 +0100

    Updated keys

--- lib/environment.dart
+++ lib/environment.dart
@@ -4,5 +4,8 @@ class Environment {
   static final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY']!;
   static final subabaseUrl = dotenv.env["SUPABASE_URL"]!;
   static final googlePlacesKey = dotenv.env["GOOGLE_PLACES_KEY"]!;
-  static final googleOAuthClient = dotenv.env["GOOGLE_OAUTH_CLIENT"]!;
+  static final googleOAuthClientAndroid =
+      dotenv.env["GOOGLE_OAUTH_CLIENT_ANDROID"]!;
+  static final googleOAuthClientIos = dotenv.env["GOOGLE_OAUTH_CLIENT_IOS"]!;
+  static final googleOAuthClientWeb = dotenv.env["GOOGLE_OAUTH_CLIENT_WEB"]!;
 }
